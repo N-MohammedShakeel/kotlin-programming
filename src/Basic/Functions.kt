e83212af228b36d17a4e8d19cde918cb0c223d52
Mohammed Shakeel <nmohammedshakeel22@gmail.com>
2025-02-05 19:34:25 +0530

    updated

---
+++ src/Basic/Functions.kt
@@ -0,0 +1,91 @@
+package Basic
+
+//Default Parameter Values and Named Arguments
+
+fun printMessage(message: String): Unit {           // no return value
+    println(message)
+}
+
+fun printMessageWithPrefix(message: String, prefix: String = "Info") { // The return type is omitted, meaning that it's actually Unit.
+    println("[$prefix] $message")
+}
+
+fun sum(x: Int, y: Int): Int {
+    return x + y
+}
+
+fun multiply(x: Int, y: Int) = x * y
+
+//Functions with vararg Parameters
+//Varargs allow you to pass any number of arguments by separating them with commas.
+
+fun printAll(vararg messages: String) {
+    for (m in messages) println(m)
+}
+
+fun printAllWithPrefix(vararg messages: String, prefix: String) {
+    for (m in messages)
+        println(prefix + m)
+}
+
+fun log(vararg entries: String) {
+    printAll(*entries)
+}
+
+
+
+fun main() {
+    printMessage("Hello")
+    printMessageWithPrefix("Hello", "Log")
+    printMessageWithPrefix("Hello")
+    printMessageWithPrefix(prefix = "Log", message = "Hello")
+    println(sum(1, 2))
+    println(multiply(2, 4))
+
+
+    printAll("Hello", "Hallo", "Salut", "Hola", "你好")
+    printAllWithPrefix(
+        "Hello", "Hallo", "Salut", "Hola", "你好",
+        prefix = "Greeting: "
+    )
+    log("Hello", "Hallo", "Salut", "Hola", "你好")
+
+
+//Infix Functions
+//Member functions and extensions with a single parameter can be turned into infix functions.
+
+//Operator Functions
+//Certain functions can be "upgraded" to operators, allowing their calls with the corresponding operator symbol.
+
+    // we can use both infix and operator in same function
+    infix operator fun Int.times(str: String): String = str.repeat(this)
+    println(2 times "Bye ")
+    println(2 * "Bye ")
+
+    val pair = "Ferrari" to "Katrina"
+    println(pair)
+
+    infix fun String.onto(other: String) = Pair(this, other)
+    val myPair = "McLaren" onto "Lucas"
+    println(myPair)
+
+    val sophia = Person("Sophia")
+    val claudia = Person("Claudia")
+    sophia likes claudia
+
+
+    operator fun String.get(range: IntRange) = substring(range)
+    val str = "Always forgive your enemies; nothing annoys them so much."
+    println(str[0..14])
+
+}
+
+
+
+class Person(val name: String) {
+    val likedPeople = mutableListOf<Person>()
+    infix fun likes(other: Person) {
+        likedPeople.add(other)
+        println(likedPeople[0].name);
+    }
+}
---
+++ src/Basic/Hello World.kt
@@ -0,0 +1,5 @@
+package Basic
+
+fun main() {
+    println("Hello, World!");
+}
